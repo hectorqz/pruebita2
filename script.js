44aff1be36d5ea4a4ca296db3044e9da9ff0eb45
hectorqz <hectorqz123@outlook.com>
2021-10-26 13:19:08 -0500

    pagina terminada si esque no le agrego algo mas xd

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let boton = document.getElementById("boton");
 boton.addEventListener("click", ()=>{
     alert("por ahora no se puede buscar nada pronto se podra!!");
 });
+
+let random = document.getElementById("random");
+
+random.addEventListener("click", ()=>{
+    alert("hola que pedo xd");
+})
