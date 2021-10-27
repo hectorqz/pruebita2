48eb9e37531566f3f9bbc33052697b6b10660a62
hectorqz <hectorqz123@outlook.com>
2021-10-27 12:22:22 -0500

    secretos y botones

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let boton = document.getElementById("boton");
+let random = document.getElementById("random");
+let yellow = document.getElementById("yellow");
+let mensaje = document.getElementById("mensaje");
 
 boton.addEventListener("click", ()=>{
     alert("por ahora no se puede buscar nada pronto se podra!!");
 });
 
-let random = document.getElementById("random");
-
 random.addEventListener("click", ()=>{
     alert("hola que pedo xd");
+})
+
+yellow.addEventListener("click",()=>{
+    mensaje.innerHTML = `hola`;
 })
